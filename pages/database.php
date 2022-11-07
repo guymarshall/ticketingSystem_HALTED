1dc48c9cfcc19f6e1e3ca541c507221634bf8910
Guy Marshall <guymarshall9@gmail.com>
2022-11-07 21:55:53 +0000

    Add database.php model file

---
+++ pages/database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database {
+    private $db_username = '';
+    private $db_password = '';
+    private $db_host = '';
+
+    public function __construct() {
+        
+    }
+    
+    public function insert($table, $data) {
+        
+    }
+
+    public function select($table, $column) {
+        
+    }
+    
+    public function update($table, $data) {
+        
+    }		
+    
+    public function delete($table, $id) {
+        
+    }		
+    
+    private function execute_query($sql) {
+        
+    }
+}

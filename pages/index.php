e99d1d7c01df111832389133341026e11c042d26
Guy Marshall <guymarshall9@gmail.com>
2022-11-07 20:14:12 +0000

    Replace index html with p tag saying this is where the tickets table will go

--- pages/index.php
+++ pages/index.php
@@ -4,10 +4,7 @@ require 'page.php';
 
 $content .= '
 <p>
-    This is my website! It includes projects I have completed. Feel free to have a look around!
-</p>
-<p>
-    If you have any suggestions for projects or change requests, visit my <a href="https://github.com/guymarshall">GitHub</a>, or <a href="mailto:guymarshall.c@gmail.com">email</a> me.
+    This is where a table of tickets will go.
 </p>';
 
 $o_index_page = new Page();

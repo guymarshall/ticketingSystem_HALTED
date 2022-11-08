c4f31115ebfdb82bd5b6841ff9327a0e283ef18c
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-08 12:27:12 +0000

    Separate index into controller and view

--- controllers/index.php
+++ controllers/index.php
@@ -2,12 +2,7 @@
 
 require 'page.php';
 
-$content = '';
-
-$content .= '
-<p>
-    This is where a table of tickets will go.
-</p>';
+require '../views/v_index.php';
 
 $o_index_page = new Page();
 $index_html = $o_index_page->create_page('All', $content);
---
+++ views/v_index.php
@@ -0,0 +1,10 @@
+<?php
+
+$content = '';
+
+$content .= '
+<p>
+    This is where a table of tickets will go.
+</p>';
+
+?>
